f5e6ce9573ed5e2ad088e1bd09798f5bc97186e6
ValeriaRabch <rabchenukv@gmail.com>
2024-03-22 16:10:42 +0200

    Add struct Worker

--- Source.cpp
+++ Source.cpp
@@ -3,10 +3,14 @@
 #include<stdio.h>
 using namespace std;
 
-
+struct Worker {
+	char name[20], surname[20];
+	char years[2];
+	char number[10];
+};
 
 int main() {
-
+	char way[] = { "d:\\Valeria\\file.txt" };
 	
 	
 	return 0;
